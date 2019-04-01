c36ac40018f9796957430c511133d6577495a015
Safiul Kabir <safiulanik@gmail.com>
2019-04-02 01:41:02 +0600

    Update urls.py, add utils.py

--- hrautomation/hrautomationapi/urls.py
+++ hrautomation/hrautomationapi/urls.py
@@ -1,13 +1,18 @@
 from rest_framework.urlpatterns import path
+from rest_framework_jwt.views import obtain_jwt_token
 from .views.request_controller import (
     RequestListAPIView,
     RequestCreateAPIView,
     RequestRetrieveUpdateDestroyAPIView
 )
+from .views.auth_controller import Signup, Login
 
 
 urlpatterns = [
     path('request/list/', RequestListAPIView.as_view(), name='request_list'),
     path('request/', RequestCreateAPIView.as_view(), name='request_create'),
     path('request/<int:pk>', RequestRetrieveUpdateDestroyAPIView.as_view(), name='request_update'),
+    path('token-auth/', obtain_jwt_token),
+    path('login/', Login.as_view()),
+    path('signup/', Signup.as_view())
 ]
