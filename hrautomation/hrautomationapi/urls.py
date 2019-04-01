c2867778072f6453913ece09ef9dd1f6a1af7bc3
Safiul Kabir <safiulanik@gmail.com>
2019-04-02 02:24:40 +0600

    Add API for getting role-wise request list

--- hrautomation/hrautomationapi/urls.py
+++ hrautomation/hrautomationapi/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('request/list/', RequestListAPIView.as_view(), name='request_list'),
     path('request/', RequestCreateAPIView.as_view(), name='request_create'),
     path('request/<int:pk>', RequestRetrieveUpdateDestroyAPIView.as_view(), name='request_update'),
+    path('request/list/<int:user_id>', RequestRetrieveUpdateDestroyAPIView.as_view(), name='request_update'),
     path('token-auth/', obtain_jwt_token),
     path('login/', Login.as_view()),
     path('signup/', Signup.as_view())
