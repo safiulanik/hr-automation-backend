949e897eb0bc285c0559c1dc36259777483d3afb
Safiul Kabir <safiulanik@gmail.com>
2019-04-06 15:49:52 +0600

    Update tests

--- hrautomation/hrautomationapi/tests/test_1_user.py
+++ hrautomation/hrautomationapi/tests/test_1_user.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from rest_framework.test import APIClient
-from hrautomation.hrautomation.local_settings import EMAIL_DOMAIN, EMAIL_DOMAIN_EXTENSION
 
 
 class TestUser(TestCase):
@@ -12,7 +11,7 @@ class TestUser(TestCase):
         user_data = {
             'first_name': 'Safiul',
             'last_name': 'Kabir',
-            'email': 'safiul@{}.{}'.format(EMAIL_DOMAIN, EMAIL_DOMAIN_EXTENSION),
+            'email': 'safiul@{}.{}'.format('gmail', 'com'),
             'role': 'engineer',
             'password': '12345678'
         }
@@ -21,7 +20,7 @@ class TestUser(TestCase):
 
     def test_02_login(self):
         login_credentials = {
-            'username': 'safiul@{}.{}'.format(EMAIL_DOMAIN, EMAIL_DOMAIN_EXTENSION),
+            'username': 'safiul@{}.{}'.format('gmail', 'com'),
             'password': '12345678'
         }
         response = self.client.post(path='/api/v1/login/', data=login_credentials)
--- hrautomation/hrautomationapi/tests/test_2_request.py
+++ hrautomation/hrautomationapi/tests/test_2_request.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from django.test import TestCase
 from rest_framework.test import APIClient
 
 
@@ -16,27 +16,26 @@ class TestRequest(TestCase):
         response = self.client.post(path='/api/v1/request/',
                                     data=request_data,
                                     HTTP_AUTHORIZATION='JWT {}'.format(self.token))
-        assert response.status_code == 200
+        self.assertEqual(response.status_code, 200)
 
     def test_02_update_req(self):
-        req_id = ''
+        req_id = 1
         updated_request_data = {
             'details': 'This is an updated demo request',
             'create_uid': 1,
             'write_uid': 1,
         }
-        response = self.client.put(path='/api/v1/request/{}'.req_id,
-                                    data=updated_request_data,
-                                    HTTP_AUTHORIZATION='JWT {}'.format(self.token))
-        assert response.status_code == 200
+        response = self.client.put(path='/api/v1/request/{}'.format(req_id),
+                                   data=updated_request_data,
+                                   HTTP_AUTHORIZATION='JWT {}'.format(self.token))
+        self.assertEqual(response.status_code, 200)
 
     def test_03_delete_req(self):
-        req_id = ''
+        req_id = 1
         response = self.client.delete(path='/api/v1/request/{}'.format(req_id),
                                       HTTP_AUTHORIZATION='JWT {}'.format(self.token))
-        assert response.status_code == 200
+        self.assertEqual(response.status_code, 200)
 
     def test_04_req_list(self):
-        response = self.client.get(path='/api/v1/request/list/',
-                                      HTTP_AUTHORIZATION='JWT {}'.format(self.token))
-        assert response.status_code == 200
+        response = self.client.get(path='/api/v1/request/list/', HTTP_AUTHORIZATION='JWT {}'.format(self.token))
+        self.assertEqual(response.status_code, 200)
